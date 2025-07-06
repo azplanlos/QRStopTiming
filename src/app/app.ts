26ccbc8d55666b8201cb6b61b322059cb94d8ea4
Andreas Zöllner <andi@go-for-fun.de>
2025-07-06 16:58:27 +0200

    QR code scan

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Scanner } from "./scanner/scanner";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [Scanner],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
+  runner(event: String) {
+    console.log(event);
+  }
   protected title = 'QRStopTiming2';
 }
---
+++ src/app/scanner/scanner.ts
@@ -0,0 +1,38 @@
+import { AfterViewInit, Component, ElementRef, OnInit, Output, ViewChild } from '@angular/core';
+import QrScanner from 'qr-scanner';
+import { BehaviorSubject, distinctUntilChanged, Observable, Subject } from 'rxjs';
+
+@Component({
+  selector: 'scanner',
+  imports: [],
+  templateUrl: './scanner.html',
+  styleUrl: './scanner.scss'
+})
+export class Scanner implements AfterViewInit, OnInit {
+  @ViewChild("qrVideo")
+  private videoElem!: ElementRef<HTMLVideoElement>;
+
+  @Output("qrCode")
+  private qrCode$: Subject<String> = new Subject<String>();
+
+  private filterSubject = new Subject<String>;
+
+  private qrScanner!: QrScanner;
+
+  ngOnInit(): void {
+      this.filterSubject.pipe(distinctUntilChanged()).subscribe(this.qrCode$);
+  }
+
+  ngAfterViewInit(): void {
+    this.qrScanner = new QrScanner(
+    this.videoElem.nativeElement,
+    result => this.filterSubject.next(result.data),
+    { /* your options or returnDetailedScanResult: true if you're not specifying any other options */ },
+);
+  }
+
+  start() {
+    this.qrScanner.start();
+  }
+
+}
